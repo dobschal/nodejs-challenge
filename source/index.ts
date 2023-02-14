77750c2df67bfe9c353bbdf420385971dfbf774c
Sascha Dobschal <s.dobschal@tdsoftware.de>
2023-02-14 11:00:04 +0100

    decrypt and unzip secure content

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,23 @@
-console.log("Here we go!");
+import { createDecipheriv } from "crypto";
+import { createReadStream, createWriteStream, readFileSync, writeFileSync } from "fs";
+import { pipeline } from "stream";
+import { createUnzip } from "zlib";
+
+const encryptedContent = readFileSync("secret.enc");
+const algorithm = "aes-256-gcm";
+const key = readFileSync("secret.key", "utf-8").substring(0, 32);
+const iv = readFileSync("iv.txt");
+const authTag = readFileSync("auth.txt");
+
+const decipher = createDecipheriv(algorithm, key, iv);
+decipher.setAuthTag(authTag);
+
+const output = Buffer.concat([decipher.update(encryptedContent), decipher.final()]);
+writeFileSync("decrypted.zip", output);
+
+const unzip = createUnzip();
+const inputStream = createReadStream("decrypted.zip");
+const outputStream = createWriteStream("decrypted");
+pipeline(inputStream, unzip, outputStream, (error) => {
+    if (error) console.error(error);
+});
