3ede35f999cf59da8cac037e91f38158edd17ade
Sascha Dobschal <s.dobschal@tdsoftware.de>
2023-02-14 20:56:12 +0100

    add sum per sentences

--- source/index.ts
+++ source/index.ts
@@ -4,9 +4,11 @@ import { pipeline } from "stream";
 import { createUnzip } from "zlib";
 
 (async function () {
+    await sumIt("clear_smaller.txt");
+    await sumPerSentence("clear_smaller.txt");
+    process.exit();
     decryptTofile("decrypted.zip");
     await unpackFile("decrypted.zip", "decrypted.txt");
-    sumIt("decrypted.txt");
 })();
 
 function decryptTofile(filename: string) {
@@ -41,43 +43,96 @@ function unpackFile(filename, outputFileName): Promise<void> {
     });
 }
 
-function sumIt(filename: string) {
-    console.log("Start summing...");
-    const t1 = Date.now();
-    const readStream = createReadStream(filename);
-    let sum = 0;
+function sumIt(filename: string): Promise<void> {
+    return new Promise((resolve) => {
+        console.log("Start summing...");
+        const t1 = Date.now();
+        const readStream = createReadStream(filename);
+        let sum = 0;
 
-    const interval = setInterval(() => {
-        console.log("Sum: ", sum);
-    }, 1000);
+        const interval = setInterval(() => {
+            console.log("Sum: ", sum);
+        }, 1000);
 
-    readStream.on('data', chunk => {
-        const str = chunk.toString("utf8");
-        const sumInChunk = str
-            .match(/[\daioeuAUOEI]/g)
-            ?.map(letter => {
-                switch (letter) {
-                    case "a": case "A": return 2;
-                    case "e": case "E": return 4;
-                    case "i": case "I": return 8;
-                    case "o": case "O": return 16;
-                    case "u": case "U": return 32;
-                    default: return Number(letter);
+        readStream.on('data', (chunk: Buffer) => {
+            const str = chunk.toString("utf8");
+            const sumInChunk = str
+                .match(/[\daioeuAUOEI]/g)
+                ?.map(letter => {
+                    switch (letter) {
+                        case "a": case "A": return 2;
+                        case "e": case "E": return 4;
+                        case "i": case "I": return 8;
+                        case "o": case "O": return 16;
+                        case "u": case "U": return 32;
+                        default: return Number(letter);
+                    }
+                })
+                .reduce(
+                    (accumulator, currentValue) => accumulator + currentValue,
+                    0
+                ) ?? 0;
+            sum += sumInChunk;
+        });
+
+        readStream.on('open', () => {
+            console.log('Stream open...');
+        });
+
+        readStream.on('end', () => {
+            console.log(`Summing done in ${Math.floor((Date.now() - t1) / 1000)}sec`, sum);
+            clearInterval(interval);
+            resolve();
+        });
+    });
+}
+
+function sumPerSentence(filename: string): Promise<void> {
+    return new Promise((resolve) => {
+        console.log("Start summing sum in sentences...");
+        const t1 = Date.now();
+        const readStream = createReadStream(filename);
+        const sumPerSentence = [0];
+
+        const interval = setInterval(() => {
+            console.log("Sentences: ", sumPerSentence.length, sumPerSentence[sumPerSentence.length - 1]);
+        }, 1000);
+
+        readStream.on('data', (chunk: Buffer) => {
+            const str = chunk.toString("utf8");
+            str.split(".").forEach((str, index) => {
+                if (index > 0) {
+                    sumPerSentence.push(0);
                 }
-            })
-            .reduce(
-                (accumulator, currentValue) => accumulator + currentValue,
-                0
-            ) ?? 0;
-        sum += sumInChunk;
-    });
+                const sumInChunk = str
+                    .match(/[\daioeuAUOEI]/g)
+                    ?.map(letter => {
+                        switch (letter) {
+                            case "a": case "A": return 2;
+                            case "e": case "E": return 4;
+                            case "i": case "I": return 8;
+                            case "o": case "O": return 16;
+                            case "u": case "U": return 32;
+                            default: return Number(letter);
+                        }
+                    })
+                    .reduce(
+                        (accumulator, currentValue) => accumulator + currentValue,
+                        0
+                    ) ?? 0;
+                sumPerSentence[sumPerSentence.length - 1] += sumInChunk;
+            });
+        });
 
-    readStream.on('open', () => {
-        console.log('Stream open...');
-    });
+        readStream.on('open', () => {
+            console.log('Stream open...');
+        });
 
-    readStream.on('end', () => {
-        console.log(`Summing done in ${Math.floor((Date.now() - t1) / 1000)}sec`, sum);
-        clearInterval(interval);
+        readStream.on('end', () => {
+            console.log(`Summing sum in sentences done in ${Math.floor((Date.now() - t1) / 1000)}sec. Sentences: `, sumPerSentence.length);
+            console.log("Sum per sentences: ", sumPerSentence);
+            clearInterval(interval);
+            resolve();
+        });
     });
 }
