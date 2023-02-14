4ea0b402518506632dfbbcf624bc58a6b5435bc1
Sascha Dobschal <s.dobschal@tdsoftware.de>
2023-02-14 11:39:33 +0100

    add summing task

--- source/index.ts
+++ source/index.ts
@@ -3,21 +3,72 @@ import { createReadStream, createWriteStream, readFileSync, writeFileSync } from
 import { pipeline } from "stream";
 import { createUnzip } from "zlib";
 
-const encryptedContent = readFileSync("secret.enc");
-const algorithm = "aes-256-gcm";
-const key = readFileSync("secret.key", "utf-8").substring(0, 32);
-const iv = readFileSync("iv.txt");
-const authTag = readFileSync("auth.txt");
+(async function () {
+    decryptTofile("decrypted.zip");
+    await unpackFile("decrypted.zip", "decrypted.txt");
+    sumIt("decrypted.txt");
+})();
 
-const decipher = createDecipheriv(algorithm, key, iv);
-decipher.setAuthTag(authTag);
+function decryptTofile(filename: string) {
+    console.log("Decrypt file...");
+    const encryptedContent = readFileSync("secret.enc");
+    const algorithm = "aes-256-gcm";
+    const key = readFileSync("secret.key", "utf-8").substring(0, 32);
+    const iv = readFileSync("iv.txt");
+    const authTag = readFileSync("auth.txt");
 
-const output = Buffer.concat([decipher.update(encryptedContent), decipher.final()]);
-writeFileSync("decrypted.zip", output);
+    const decipher = createDecipheriv(algorithm, key, iv);
+    decipher.setAuthTag(authTag);
 
-const unzip = createUnzip();
-const inputStream = createReadStream("decrypted.zip");
-const outputStream = createWriteStream("decrypted");
-pipeline(inputStream, unzip, outputStream, (error) => {
-    if (error) console.error(error);
-});
+    const output = Buffer.concat([decipher.update(encryptedContent), decipher.final()]);
+    writeFileSync(filename, output);
+    console.log("Decrypt done.");
+}
+
+function unpackFile(filename, outputFileName): Promise<void> {
+    return new Promise((resolve, reject) => {
+        console.log("Unzip file...");
+        const unzip = createUnzip();
+        const inputStream = createReadStream(filename);
+        const outputStream = createWriteStream(outputFileName);
+        pipeline(inputStream, unzip, outputStream, (error) => {
+            if (error) return reject(error);
+            resolve();
+            console.log("Unzip file done.");
+        });
+    });
+}
+
+function sumIt(filename: string) {
+    const readStream = createReadStream(filename);
+    let sum = 0;
+
+    readStream.on('data', chunk => {
+        const str = chunk.toString("utf8");
+        const sumInChunk = str
+            .match(/[\daioeuAUOEI]/g)
+            ?.map(letter => {
+                switch (letter) {
+                    case "a": case "A": return 2;
+                    case "e": case "E": return 4;
+                    case "i": case "I": return 8;
+                    case "o": case "O": return 16;
+                    case "u": case "U": return 32;
+                    default: return Number(letter);
+                }
+            })
+            .reduce(
+                (accumulator, currentValue) => accumulator + currentValue,
+                0
+            ) ?? 0;
+        sum += sumInChunk;
+    });
+
+    readStream.on('open', () => {
+        console.log('Summing...');
+    });
+
+    readStream.on('end', () => {
+        console.log("Summing done: ", sum);
+    });
+}
